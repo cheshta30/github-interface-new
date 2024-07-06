86cb225f5bd7210b4d029983b873efa8d07f2b49
cheshta30 <cheshta.nagpal3009@gmail.com>
2024-07-06 23:08:52 +0530

    basic tanstack

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
 import "./App.css";
 import Avatar from "@mui/material/Avatar";
 import SearchBar from "./components/SearchBar";
@@ -7,6 +8,8 @@ import { IoLocationSharp } from "react-icons/io5";
 import { GoLink } from "react-icons/go";
 import { PiBuildingOfficeLight } from "react-icons/pi";
 import { MdOutlineLightMode } from "react-icons/md";
+
+const queryClient = new QueryClient()
 
 function fetchGithubData(userName) {
   return fetch(`https://api.github.com/users/${userName}`)
@@ -20,136 +23,126 @@ function fetchGithubData(userName) {
 
 export default function App() {
   const [userName, setUserName] = useState("");
-  const [profileData, setProfileData] = useState(null);
-  const [error, setError] = useState(null);
 
-  const fetchProfileData = async (userName) => {
-    try {
-      const data = await fetchGithubData(userName);
-      setUserName(data.login);
-      setProfileData(data);
-      setError(null);
-    } catch (error) {
-      console.error("Error fetching profile data:", error);
-      setUserName("");
-      setProfileData(null);
-      setError(error.message);
-    }
-  };
+  const { isLoading, error, data: profileData } = useQuery({
+    queryKey: ['githubUser', userName],
+    queryFn: () => fetchGithubData(userName),
+    enabled: !!userName,
+  })
 
-  const handleSubmit = (userName) => {
-    if (!userName) {
+  const handleSubmit = (newUserName) => {
+    if (!newUserName) {
       alert("Username can't be null");
       return;
     }
-    fetchProfileData(userName);
+    setUserName(newUserName);
   };
 
-
   return (
-    <div id="outline">
-      <nav id="nav">
-        <h3>devfinder</h3>
-        <pre>Dark <MdOutlineLightMode /></pre>
-      </nav>
-      <br />
-      <SearchBar handleSubmit={handleSubmit} />
-      <br />
-      <div id="card">
-        {profileData && (
-          <div id="profile">
-            <Avatar sx={{ width: 90, height: 90 }} alt="Avatar" src={profileData.avatar_url} />
-            <div id="profile-text">
-              <div id="name-joined">
-                <h2>{profileData.login}</h2>
-                <h4>Joined: {new Date(profileData.created_at).toLocaleDateString()}</h4>
-              </div>
-              <h3>@{profileData.login}</h3>
-            </div>
-          </div>
-        )}
-        <div id="other_content">
-          {profileData ? (
+    <QueryClientProvider client={queryClient}>
+      <div id="outline">
+        <nav id="nav">
+          <h3>devfinder</h3>
+          <pre>Dark <MdOutlineLightMode /></pre>
+        </nav>
+        <br />
+        <SearchBar handleSubmit={handleSubmit} />
+        <br />
+        <div id="card">
+          {isLoading ? (
+            <div>Loading...</div>
+          ) : error ? (
+            <div>An error occurred: {error.message}</div>
+          ) : profileData ? (
             <>
-              <p>{profileData.bio ? profileData.bio : "This profile has no bio"}</p>
-              <br />
-              <div id="rectangle">
-                <span>
-                  <div>
-                    Followers: <br />
-                    <pre>  {profileData.followers}</pre>
-                   </div>
-                </span>
-                <span>
-                  <div>
-                    Following: <br />
-                    <pre>   {profileData.following}</pre>
+              <div id="profile">
+                <Avatar sx={{ width: 90, height: 90 }} alt="Avatar" src={profileData.avatar_url} />
+                <div id="profile-text">
+                  <div id="name-joined">
+                    <h2>{profileData.login}</h2>
+                    <h4>Joined: {new Date(profileData.created_at).toLocaleDateString()}</h4>
                   </div>
-                </span>
-                <span>
-                  <div>
-                    Repos: <br />
-                    <pre>  {profileData.public_repos}</pre>
-                    
-                  </div>
-                </span>
+                  <h3>@{profileData.login}</h3>
+                </div>
               </div>
-              <br />
-              <div id="Other1">
-                <span>
-                  {profileData.location ? (
-                    <>
-                      <IoLocationSharp /> {profileData.location}
-                    </>
-                  ) : (
-                    <>
-                      <IoLocationSharp /> Not Available
-                    </>
-                  )}
-                </span>
-                <span>
-                  {profileData.twitter_username ? (
-                    <>
-                      <FaTwitter /> {profileData.twitter_username}
-                    </>
-                  ) : (
-                    <>
-                      <FaTwitter /> Not Available
-                    </>
-                  )}
-                </span>
-              </div>
-              <br />
-              <div id="Other2">
-                <span>
-                  {profileData.blog ? (
-                    <>
-                      <GoLink /> {profileData.blog}
-                    </>
-                  ) : (
-                    <>
-                      <GoLink /> Not Available
-                    </>
-                  )}
-                </span>
-                <span>
-                  {profileData.company ? (
-                    <>
-                      <PiBuildingOfficeLight /> {profileData.company}
-                    </>
-                  ) : (
-                    <>
-                      <PiBuildingOfficeLight /> Not Available
-                    </>
-                  )}
-                </span>
+              <div id="other_content">
+                <p>{profileData.bio ? profileData.bio : "This profile has no bio"}</p>
+                <br />
+                <div id="rectangle">
+                  <span>
+                    <div>
+                      Followers: <br />
+                      <pre>  {profileData.followers}</pre>
+                    </div>
+                  </span>
+                  <span>
+                    <div>
+                      Following: <br />
+                      <pre>   {profileData.following}</pre>
+                    </div>
+                  </span>
+                  <span>
+                    <div>
+                      Repos: <br />
+                      <pre>  {profileData.public_repos}</pre>
+                    </div>
+                  </span>
+                </div>
+                <br />
+                <div id="Other1">
+                  <span>
+                    {profileData.location ? (
+                      <>
+                        <IoLocationSharp /> {profileData.location}
+                      </>
+                    ) : (
+                      <>
+                        <IoLocationSharp /> Not Available
+                      </>
+                    )}
+                  </span>
+                  <span>
+                    {profileData.twitter_username ? (
+                      <>
+                        <FaTwitter /> {profileData.twitter_username}
+                      </>
+                    ) : (
+                      <>
+                        <FaTwitter /> Not Available
+                      </>
+                    )}
+                  </span>
+                </div>
+                <br />
+                <div id="Other2">
+                  <span>
+                    {profileData.blog ? (
+                      <>
+                        <GoLink /> {profileData.blog}
+                      </>
+                    ) : (
+                      <>
+                        <GoLink /> Not Available
+                      </>
+                    )}
+                  </span>
+                  <span>
+                    {profileData.company ? (
+                      <>
+                        <PiBuildingOfficeLight /> {profileData.company}
+                      </>
+                    ) : (
+                      <>
+                        <PiBuildingOfficeLight /> Not Available
+                      </>
+                    )}
+                  </span>
+                </div>
               </div>
             </>
-          ) : (
-            console.log(error)
-          )}
+          ) : null}
         </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
